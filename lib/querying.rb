c7f7b8597e6e743836c3e82050bad18438192991
Andy Adams <andrewdcadams@gmail.com>
2020-09-23 22:24:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT projects.title, SUM(pledges.amount) 
+  FROM projects 
+  LEFT OUTER JOIN pledges ON projects.id = pledges.project_id 
+  GROUP BY projects.title
+  ORDER BY projects.title ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
