118f63aabc83b5e19334f5170e3f3237cc47522e
Andy Adams <andrewdcadams@gmail.com>
2020-09-24 02:24:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
     ON characters.author_id = authors.id
     GROUP BY series.title 
     ORDER BY COUNT(characters.species) DESC 
-    LIMIT 1"
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
