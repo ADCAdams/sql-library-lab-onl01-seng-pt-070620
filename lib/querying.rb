1f046c803c21352e0d4c2ae12bf1d225358ebec0
Andy Adams <andrewdcadams@gmail.com>
2020-09-24 02:36:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,12 +43,12 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, subgenres.name
+  "SELECT characters.name
   FROM characters
   JOIN character_books 
   ON character_books.character_id = characters.id
   JOIN books
   ON character_books.book_id = book.id
   GROUP BY characters.name
-  ORDER BY COUNT(;"
+  ORDER BY COUNT(books.title) DESC;"
 end
